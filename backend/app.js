34acce8b338061aeec27902da21f583f352922f4
brajeshmishra825 <brajeshmishra825@gmail.com>
2022-01-30 19:20:05 +0530

    Created QR Code

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,19 @@ const busboy = require("connect-busboy");
 const errorMiddleware = require("./middleware/error");
 const helmet = require("helmet");
 const compression = require("compression");
+const qr = require("qr-image");
 
 // Middleware
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
 
 app.use(
-  busboy({
-    highWaterMark: 2 * 1024 * 1024,
-  })
+    busboy({
+        highWaterMark: 2 * 1024 * 1024,
+    })
 );
 
 // Route imports
@@ -25,11 +27,23 @@ const document = require("./routes/documentRoute");
 const folder = require("./routes/folderRoute");
 
 app.get("/", (req, res) => {
-  res.json({
-    "Drive API": {
-      "View Documentation": "/documentation/view",
-    },
-  });
+    res.json({
+        "Drive API": {
+            "View Documentation": "/documentation/view",
+        },
+    });
+});
+
+app.get("/createQRCode", (req, res) => {
+    var text = req.query.text || "";
+    try {
+        var img = qr.image(text, { size: 10 });
+        res.writeHead(200, { "Content-Type": "image/png" });
+        img.pipe(res);
+    } catch (e) {
+        res.writeHead(414, { "Content-Type": "text/html" });
+        res.end("<h1>414 Request-URI Too Large</h1>");
+    }
 });
 
 app.use("/user-service/", user);
--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -1,10 +1,10 @@
 const documentation = require("../Data/documentation");
 
 class DocumentController {
-  constructor() {}
-  getDocument = async (req, res) => {
-    res.json(documentation);
-  };
+    constructor() {}
+    getDocument = async(req, res) => {
+        res.json(documentation);
+    };
 }
 
 module.exports = DocumentController;
