1fbb81f14c12eddeb44e83af22ebffddf7ce5c53
brajeshmishra825 <brajeshmishra825@gmail.com>
2022-02-03 18:12:43 +0530

    Send Email Modified

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,22 +1,37 @@
 const nodeMailer = require("nodemailer");
+// const sendEmail = async(options) => {
+//     const transporter = nodeMailer.createTransport({
+//         service: process.env.SMPT_SERVICE,
+//         host: process.env.SMTP_HOST,
+//         auth: {
+//             user: process.env.SMPT_MAIL,
+//             pass: process.env.SMPT_PASSWORD,
+//         },
+//     });
+//     const mailOptions = {
+//         from: process.env.SMPT_MAIL,
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// };
+
+const sendmail = require("sendmail")();
+
 const sendEmail = async(options) => {
-    const transporter = nodeMailer.createTransport({
-        service: process.env.SMPT_SERVICE,
-        host: "smtp.gmail.com",
-        auth: {
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
+    await sendmail({
+            from: process.env.SMPT_MAIL,
+            to: options.email,
+            subject: options.subject,
+            html: "Mail of test sendmail ",
         },
-    });
-
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
-
-    await transporter.sendMail(mailOptions);
+        function(err, reply) {
+            console.log(err && err.stack);
+            console.dir(reply);
+        }
+    );
 };
 
 module.exports = sendEmail;
